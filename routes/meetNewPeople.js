fd3a39bb5b8409aed38985498c0e389a1a254681
Mike-Griffin <mike_griffin@comcast.net>
2016-02-17 20:30:19 -0800

    Removed console log statements

--- routes/meetNewPeople.js
+++ routes/meetNewPeople.js
@@ -8,23 +8,18 @@ exports.view = function(req, res) {
   var idVal = req.query.idVal;
 
   if(difficulty !== undefined && desc !== undefined && idVal != undefined) {
-    console.log("Id found");
-    console.log("Inserted id: " + idVal);
+
 
     for(var i = 0; i < 2; i++) {
       for(var j = 0; j < 3; j++) {
         for(var k = 0; k < data.status[i].activity[j].descriptions.length; k++) {
-          console.log("Data's id: " + data.status[i].activity[j].descriptions[k].id);
 
           if(data.status[i].activity[j].descriptions[k].id == idVal) {
-            console.log("match found");
             if(j === Number(difficulty)) {
               data.status[i].activity[j].descriptions[k].name = desc;
-              console.log("Change name");
             }
             else {
               data.status[i].activity[j].descriptions.splice(k,1);
-              console.log("Change difficulty");
               data.status[0].activity[Number(difficulty)].descriptions.push({
                 'id': idVal,
                 'name': desc
@@ -42,7 +37,6 @@ exports.view = function(req, res) {
   }
 
   else if (difficulty !== undefined && desc !== undefined) {
-    console.log("Trying to insert");
     var id =  Math.floor((Math.random() * 10000) + 1);
 
     data.status[0].activity[Number(difficulty)].descriptions.push({
