dac435b85eb7b3652acdd1f774587e67abe95b0e
Mike-Griffin <mike_griffin@comcast.net>
2016-03-03 20:30:05 -0800

    Points now being accumulated

--- routes/meetNewPeople.js
+++ routes/meetNewPeople.js
@@ -1,4 +1,5 @@
 var data = require("../activity.json");
+
 var curList = 0;
 exports.view = function(req, res) {
 
@@ -6,12 +7,18 @@ exports.view = function(req, res) {
   var difficulty = req.query['level-difficulty'];
   var desc = req.query.ActivityDescription;
   var idVal = req.query.idVal;
+  var totalPoints= req.query.totalPoints;
+  var difficultyPoints = req.query.difficultyPoints;
 
   console.log(difficulty);
   console.log(desc);
   console.log(idVal);
+  console.log(difficultyPoints);
+  console.log(totalPoints);
 
+// delete
   if(idVal !== undefined && difficulty === undefined && desc === undefined) {
+    console.log("delete");
     for(var i = 0; i < 2; i++) {
       for(var j = 0; j < 3; j++) {
         for(var k = 0; k < data.status[i].activity[j].descriptions.length; k++) {
@@ -23,8 +30,18 @@ exports.view = function(req, res) {
       }
     }
   }
-
-  else if(idVal !== undefined && difficulty === undefined && desc !== undefined) {
+// mark as completed
+  else if(idVal !== undefined && difficulty === undefined && desc !== undefined
+          && difficultyPoints !== undefined && totalPoints !== undefined) {
+    var difficultyInt = parseInt(difficultyPoints);
+    var totalInt = parseInt(totalPoints);
+    var totalInt = totalInt + difficultyInt;
+    totalPoints = totalInt.toString();
+    data.status[0].points = totalPoints;
+    console.log(data.status[0].points);
+    console.log("Mark as completed");
+    console.log(difficultyPoints);
+    console.log(totalPoints);
     for(var i = 0; i < 2; i++) {
       for(var j = 0; j < 3; j++) {
         for(var k = 0; k < data.status[i].activity[j].descriptions.length; k++) {
@@ -43,16 +60,18 @@ exports.view = function(req, res) {
 
   else if(difficulty !== undefined && desc !== undefined && idVal != undefined) {
 
-
+    console.log("Edit");
     for(var i = 0; i < 2; i++) {
       for(var j = 0; j < 3; j++) {
         for(var k = 0; k < data.status[i].activity[j].descriptions.length; k++) {
 
           if(data.status[i].activity[j].descriptions[k].id == idVal) {
             if(j === Number(difficulty)) {
+              console.log("Change name");
               data.status[i].activity[j].descriptions[k].name = desc;
             }
             else {
+              console.log("Change difficulty");
               data.status[i].activity[j].descriptions.splice(k,1);
               data.status[0].activity[Number(difficulty)].descriptions.push({
                 'id': idVal,
@@ -72,7 +91,7 @@ exports.view = function(req, res) {
 
   else if (difficulty !== undefined && desc !== undefined) {
     var id =  Math.floor((Math.random() * 10000) + 1);
-console.log("insert"); 
+console.log("insert");
     data.status[0].activity[Number(difficulty)].descriptions.push({
       'id': id,
       'name': desc
