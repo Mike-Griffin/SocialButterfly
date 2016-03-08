c6e99f890cbb4ca50dd8aca8118156c1881e093b
Mike-Griffin <mike_griffin@comcast.net>
2016-03-07 22:36:17 -0800

    Reset points on logout

--- routes/login.js
+++ routes/login.js
@@ -1,3 +1,5 @@
+var data = require("../activity.json");
+
 exports.view = function(req, res){
   res.render('login', {});
 };
--- routes/quizOne.js
+++ routes/quizOne.js
@@ -1,3 +1,8 @@
+var data = require("../activity.json");
+
+
 exports.view = function(req, res){
+  data.status[0].points = "0";
+
   res.render('quizOne', {});
 };
